fffa576c9d677b700b174a0bd0b79a143ef612ab
Luis C <zs21013875@estudiantes.uv.mx>
2024-04-18 22:28:21 -0600

    Client implementation

---
+++ app/src/main/java/calculadoragrpc/cliente/Cliente.java
@@ -0,0 +1,74 @@
+package calculadoragrpc.cliente;
+
+import javax.swing.JOptionPane;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+import com.proto.calculadora.CalculadoraServiceGrpc;
+import com.proto.calculadora.Calculadora.MulRequest;
+import com.proto.calculadora.Calculadora.MulResponse;
+import com.proto.calculadora.Calculadora.DivRequest;
+import com.proto.calculadora.Calculadora.DivResponse;
+import com.proto.calculadora.Calculadora.SumRequest;
+import com.proto.calculadora.Calculadora.SumResponse;
+import com.proto.calculadora.Calculadora.ResRequest;
+import com.proto.calculadora.Calculadora.ResResponse;
+
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+public class Cliente {
+    public static void main(String[] args) {
+        int puerto = 7878;
+        String host = "localhost";
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(host, puerto).usePlaintext().build();
+        CalculadoraServiceGrpc.CalculadoraServiceBlockingStub stub = CalculadoraServiceGrpc.newBlockingStub(channel);
+
+        while (true) {
+            String opt = JOptionPane.showInputDialog(
+                    "Calcular\n" +
+                            "Suma............. (1)\n" +
+                            "Resta............ (2)\n" +
+                            "Multiplicación... (3)\n" +
+                            "División......... (4)\n" +
+                            "Cancelar para salir"
+            );
+
+            if (opt == null) {
+                break;
+            }
+
+            int a = Integer.parseInt(JOptionPane.showInputDialog("Ingrese a"));
+            int b = Integer.parseInt(JOptionPane.showInputDialog("Ingrese b"));
+
+            switch (opt) {
+                case "1":
+                    SumRequest sumRequest = SumRequest.newBuilder().setA(a).setB(b).build();
+                    SumResponse sumResponse = stub.sumar(sumRequest);
+                    JOptionPane.showMessageDialog(null, a + " + " + b + " = " + sumResponse.getRes() );
+                    break;
+                case "2":
+                    ResRequest resRequest = ResRequest.newBuilder().setA(a).setB(b).build();
+                    ResResponse resResponse = stub.restar(resRequest);
+                    JOptionPane.showMessageDialog(null, a + " - " + b + " = " + resResponse.getRes() );
+                    break;
+                case "3":
+                    MulRequest mulRequest = MulRequest.newBuilder().setA(a).setB(b).build();
+                    MulResponse mulResponse = stub.multiplicar(mulRequest);
+                    JOptionPane.showMessageDialog(null, a + " * " + b + " = " + mulResponse.getRes() );
+                    break;
+                case "4":
+                    DivRequest divRequest = DivRequest.newBuilder().setA(a).setB(b).build();
+                    DivResponse divResponse = stub.dividir(divRequest);
+                    JOptionPane.showMessageDialog(null, a + " / " + b + " = " + divResponse.getRes() );
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Ingrese una opción válida.");
+                    break;
+            }
+        }
+    }
+}
